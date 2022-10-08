fa28c49da9522ebd54da40302e0943e218b670fe
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-10-08 18:28:32 -0300

    feat: add lógica para mostrar mensagem de erro

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,17 +1,21 @@
 let caracter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B','C', 'D', 'E', 'F', 'G', 'H', 'I',
  'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2,
-  3, 4, 5, 6, 7, 8, 9, '!', '@', '#', '$', '&', '%', '{', '}', '[', ']', '?', ';'] 
+  3, 4, 5, 6, 7, 8, 9, '!', '@', '#', '$', '&', '%', '{', '}', '[', ']', '?', ';', '(', ')', '*'] 
 
 
 function gerarSenha() {
   let senha = [];
   tamanhoSenha = document.querySelector('input').value;
   if (tamanhoSenha <= 0) {
-    alert('O tamanho da senha tem que ser maior que zero!')
+    document.querySelector('#tamanho').innerHTML = 'O tamanho da senha tem que ser maior que zero!';
+    document.getElementById('tamanho').style.visibility = 'visible';
   } else if (tamanhoSenha > 100) {
-    alert('O tamanho da senha tem que ser menor que cem!')
+    document.querySelector('#tamanho').innerHTML = 'O tamanho máximo da senha é 100!';
+    document.getElementById('tamanho').style.visibility = 'visible';
   } else {
+    document.querySelector('#tamanho').innerHTML = '';
+    document.getElementById('tamanho').style.visibility = 'hidden';
     for (let index = 0; index < tamanhoSenha; index++) {
       senha.push(caracter[Math.floor((Math.random() * caracter.length))])
     }
